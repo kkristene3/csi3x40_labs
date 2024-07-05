4832306f5a9cfefb18355d58a03188c407aeebb0
kkristene3 <kkristene3@gmail.com>
2024-07-04 21:02:37 -0400

    Hook button with Ajax call

--- yatzy/public/api.php
+++ yatzy/public/api.php
@@ -1,4 +1,4 @@
 <?php
 require_once('_config.php');
 header("Content-Type: application/json");
-echo json_encode(["version" => "0.9"]);
+echo json_encode(["version" => "1.0"]);
--- yatzy/public/index.php
+++ yatzy/public/index.php
@@ -34,6 +34,18 @@ require_once('_config.php');
   const output = document.getElementById("output");
   const version = document.getElementById("version");
   version.onclick = function(e) {
-    output.innerHTML = "Look up version clicked";
-  }
+
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+        if (xmlhttp.status == 200) {
+          output.innerHTML = xmlhttp.responseText;
+        }
+      }
+    };
+
+    xmlhttp.open("GET", "/api.php", true);
+    xmlhttp.send();
+    }
 </script>
