0566c261d2f8ff031627354079b04eaad817f16e
kkristene3 <kkristene3@gmail.com>
2024-07-04 22:58:19 -0400

    Attempt adding Slim, but remove because it didn't work

--- yatzy/public/index.php
+++ yatzy/public/index.php
@@ -19,26 +19,32 @@ for ($i = 0; $i < 3; $i++) {
 ?>
 
 <!DOCTYPE html>
-<br><br>
+<head>
+    <script type="text/javascript" src="/assets/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
 <div id="die1">--</div>
 <button id="roll">Roll</button>
+</body>
 
 <script>
-const die1 = document.getElementById("die1");
-const roll = document.getElementById("roll");
-roll.onclick = function(e) {
+  const die1 = document.getElementById("die1");
+  const roll = document.getElementById("roll");
+  roll.onclick = function(e) {
 
-  const xmlhttp = new XMLHttpRequest();
+    const xmlhttp = new XMLHttpRequest();
 
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-      if (xmlhttp.status == 200) {
-        die1.innerHTML = xmlhttp.responseText;
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+        if (xmlhttp.status == 200) {
+          die1.innerHTML = xmlhttp.responseText;
+        }
       }
-    }
-  };
+    };
 
-  xmlhttp.open("GET", "/api.php?action=roll", true);
-  xmlhttp.send();
-}
+    xmlhttp.open("GET", "/api.php?action=roll", true);
+    xmlhttp.send();
+  }
 </script>
+<br><br>
