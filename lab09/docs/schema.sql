01ea81953c80070cd5f0aad5c85f1179e2ce7872
kkristene3 <kkristene3@gmail.com>
2024-07-19 22:46:32 -0400

    Add sample data to each table for testing

--- lab09/docs/schema.sql
+++ lab09/docs/schema.sql
@@ -1,8 +1,13 @@
 create database emergency_waitlist;
 use emergency_waitlist;
 
+/* Create the tables */ 
 CREATE TABLE administrator;
 CREATE TABLE patient;
 CREATE TABLE staff;
 CREATE TABLE PatientRecord;
 CREATE TABLE queue;
+
+/* Create sample data */
+INSERT INTO administrator (username, loginCode) VALUES ('admin', '123');
+INSERT INTO patient (name, patientID, loginCode) VALUES ('John Doe', '1', '001');
