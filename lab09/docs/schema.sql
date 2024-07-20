a7def7c58576d4f52385dc97955da14404180e86
kkristene3 <kkristene3@gmail.com>
2024-07-19 22:31:34 -0400

    Create SQL schema

---
+++ lab09/docs/schema.sql
@@ -0,0 +1,8 @@
+create database emergency_waitlist;
+use emergency_waitlist;
+
+CREATE TABLE administrator;
+CREATE TABLE patient;
+CREATE TABLE staff;
+CREATE TABLE PatientRecord;
+CREATE TABLE queue;
